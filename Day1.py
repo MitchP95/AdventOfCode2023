c3f644054de34aefded9a2b6f8fec204bbc7de46
Mitchell Puersten <mitch.puersten@gmail.com>
2023-12-01 17:15:56 +0000

    Part way through part 2, tweaking algorithm.

--- Day1.py
+++ Day1.py
@@ -1,9 +1,10 @@
 from Utils.FileReader import read_lines_from_file
+from Utils.Converters import written_number_to_digit
 
 lines = read_lines_from_file('Inputs/Day1.txt')
 
+# Part 1
 total = 0
-
 for line in lines:
     hasFirst = False
     firstValue = ''
@@ -18,6 +19,33 @@ for line in lines:
             if (line[i].isdigit()):
                 lastValue = line[i]
 
+    if (lastValue == ''):
+        lastValue = firstValue
+
+    total += int(firstValue + lastValue)
+    
+print(total)
+
+# Part 2
+total = 0
+for line in lines:
+    hasFirst = False
+    firstValue = ''
+    lastValue = ''
+
+    startPointer = 0
+    for i in range(len(line)):
+        if (written_number_to_digit(line[startPointer:i]) != -1):
+            if not hasFirst:
+                firstValue = written_number_to_digit(line[startPointer:i])
+                hasFirst = True
+            else:
+                lastValue = written_number_to_digit(line[startPointer:i])
+
+            startPointer = i + 1
+        else:
+            print(line[startPointer:i])
+        
     if (lastValue == ''):
         lastValue = firstValue
 
