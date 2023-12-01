0536b29858aa90aef143041ce3436427740a80ad
Mitchell Puersten <mitch.puersten@gmail.com>
2023-12-01 19:00:18 +0000

    Added missing converters file.

---
+++ Utils/Converters.py
@@ -0,0 +1,23 @@
+def written_number_to_digit(written_number):
+    result = -1
+
+    if written_number == 'one':
+        result = 1
+    elif written_number == 'two':
+        result = 2
+    elif written_number == 'three':
+        result = 3
+    elif written_number == 'four':
+        result = 4
+    elif written_number == 'five':
+        result = 5
+    elif written_number == 'six':
+        result = 6
+    elif written_number == 'seven':
+        result = 7
+    elif written_number == 'eight':
+        result = 8
+    elif written_number == 'nine':
+        result = 9
+
+    return result
